42513866dfeba59f6240219f0e7db06cddc2dc9a
wangyou2026 <540726178@qq.com>
2026-04-27 07:05:54 +0800

    fix: 修正 compose-bom 版本为 2024.02.00

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2")
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.07.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
